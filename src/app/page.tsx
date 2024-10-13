74b4b63602608c0d941ff5d7e5ac241886b90a51
Kazuki Minakawa <kazuki.minakawa@funlab.jp>
2024-10-13 19:35:10 +0900

    add tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
 
 const Top: FC = async () => {
-  return <h1>Hello, Next.js! Updated!</h1>
+  return <h1 className='text-blue-500'>Hello, Next.js! Updated!</h1>
 }
 export default Top
